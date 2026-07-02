7543ed217d2278cb816139341987468d214b7c09
Christoph Gietl <christophgietl@users.noreply.github.com>
2026-07-02 22:42:37 +0200

    Refactor conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,31 @@
-import typing
+from typing import TYPE_CHECKING
 
 import pytest
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
-from app.database import create_and_initialize_engine, get_async_engine
+from app.database import create_and_initialize_engine
 from app.main import app
 
-if typing.TYPE_CHECKING:
+if TYPE_CHECKING:
     from collections.abc import AsyncGenerator, Generator
     from pathlib import Path
 
     from fastapi import FastAPI
 
 
+@pytest.fixture
+def _app(_engine: AsyncEngine) -> Generator[FastAPI]:  # pyright: ignore[reportUnusedFunction]
+    app.state.async_engine = _engine
+    yield app
+    del app.state.async_engine
+
+
 @pytest.fixture(autouse=True)
-def _set_database_url(monkeypatch: pytest.MonkeyPatch, tmp_path: Path) -> None:  # pyright: ignore[reportUnusedFunction]
-    database_file = tmp_path / "database.sqlite"
-    monkeypatch.setenv("DATABASE_URL", f"sqlite+aiosqlite:///{database_file}")
+def _database_url(monkeypatch: pytest.MonkeyPatch, tmp_path: Path) -> None:  # pyright: ignore[reportUnusedFunction]
+    file = tmp_path / "database.sqlite"
+    url = f"sqlite+aiosqlite:///{file}"
+    monkeypatch.setenv("DATABASE_URL", url)
 
 
 @pytest.fixture
@@ -29,14 +37,10 @@ async def _engine() -> AsyncGenerator[AsyncEngine]:  # pyright: ignore[reportUnu
 
 
 @pytest.fixture
-def _app(_engine: AsyncEngine) -> Generator[FastAPI]:  # pyright: ignore[reportUnusedFunction]
-    app.dependency_overrides = {get_async_engine: lambda: _engine}
-    yield app
-    app.dependency_overrides.clear()
-
-
-@pytest.fixture
 async def client(_app: FastAPI) -> AsyncGenerator[AsyncClient]:
+    # The database setup logic for `app` lives in its `lifespan` function.
+    # Unfortunately, `ASGITransport` and `AsyncClient` do not run `lifespan` events.
+    # Therefore, we use the `_app` fixture which takes care of the database setup.
     transport = ASGITransport(_app)
     async with AsyncClient(
         base_url="http://test", follow_redirects=True, transport=transport
@@ -46,5 +50,5 @@ async def client(_app: FastAPI) -> AsyncGenerator[AsyncClient]:
 
 @pytest.fixture
 async def session(_engine: AsyncEngine) -> AsyncGenerator[AsyncSession]:
-    async with AsyncSession(_engine) as async_session:
-        yield async_session
+    async with AsyncSession(_engine) as session:
+        yield session
