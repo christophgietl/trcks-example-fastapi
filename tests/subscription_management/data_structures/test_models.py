15c7669a80cf2491c16aeb03f67d664d2a18b0ba
Christoph Gietl <christophgietl@users.noreply.github.com>
2026-07-16 23:25:59 +0200

    Refactor TestSubscriptionModel

--- tests/subscription_management/data_structures/test_models.py
+++ tests/subscription_management/data_structures/test_models.py
@@ -3,13 +3,19 @@ from typing import TYPE_CHECKING
 from uuid import uuid7
 
 import pytest
-from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 
-from subscription_management.data_structures.models import (
-    ProductModel,
-    SubscriptionModel,
-    UserModel,
+from subscription_management.data_structures.domain.product import Product
+from subscription_management.data_structures.domain.subscription import (
+    SubscriptionWithUserIdAndProductId,
+)
+from subscription_management.data_structures.domain.user import User
+from subscription_management.data_structures.models import SubscriptionModel, UserModel
+from subscription_management.testing.helpers import (
+    insert_products,
+    insert_subscriptions,
+    insert_users,
+    select_subscriptions,
 )
 
 if TYPE_CHECKING:
@@ -20,21 +26,19 @@ class TestSubscriptionModel:
     async def test_foreign_key_relationships_are_validated_on_insert(
         self, session: AsyncSession
     ) -> None:
-        product_id = uuid7()
-        product_model = ProductModel(
-            id=product_id,
+        product = Product(
+            id=uuid7(),
             monthly_fee_in_euros=Decimal("9.99"),
             name="Product 1",
             status="published",
         )
-        async with session.begin():
-            session.add(product_model)
+        await insert_products(session, product)
 
         nonexistent_user_id = uuid7()
         subscription_model = SubscriptionModel(
             id=uuid7(),
             is_active=True,
-            product_id=product_id,
+            product_id=product.id,
             user_id=nonexistent_user_id,
         )
         with pytest.raises(IntegrityError):
@@ -44,32 +48,27 @@ class TestSubscriptionModel:
     async def test_subscriptions_are_deleted_on_user_deletion(
         self, session: AsyncSession
     ) -> None:
-        product_id = uuid7()
-        user_id = uuid7()
-        models = (
-            ProductModel(
-                id=product_id,
-                monthly_fee_in_euros=Decimal("9.99"),
-                name="Product 1",
-                status="published",
-            ),
-            UserModel(id=user_id, email="user@example.com"),
-            SubscriptionModel(
-                id=uuid7(),
-                is_active=True,
-                product_id=product_id,
-                user_id=user_id,
-            ),
+        product = Product(
+            id=uuid7(),
+            monthly_fee_in_euros=Decimal("9.99"),
+            name="Product 1",
+            status="published",
         )
-        async with session.begin():
-            session.add_all(models)
+        await insert_products(session, product)
+        user = User(id=uuid7(), email="user@example.com")
+        await insert_users(session, user)
+        subscription = SubscriptionWithUserIdAndProductId(
+            id=uuid7(),
+            is_active=True,
+            product_id=product.id,
+            user_id=user.id,
+        )
+        await insert_subscriptions(session, subscription)
 
         async with session.begin():
-            user_model = await session.get(UserModel, user_id)
+            user_model = await session.get(UserModel, user.id)
             assert user_model is not None
             await session.delete(user_model)
 
-        async with session.begin():
-            user_id_scalars = await session.scalars(select(SubscriptionModel.user_id))
-            user_ids = user_id_scalars.all()
-        assert user_ids == []
+        subscriptions = await select_subscriptions(session)
+        assert subscriptions == ()
